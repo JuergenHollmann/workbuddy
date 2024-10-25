abb3f423e34be0952e52f54f13a07b59cdef655a
Jürgen Hollmann <h.juergen.hollmann@gmail.com>
2024-10-25 10:05:10 +0200

    implemented new color

--- lib/config/wb_colors.dart
+++ lib/config/wb_colors.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
 
+// ----------------------------- Parbpalette blau ---
 const Color wbBackgroundBlue =
     Color.fromARGB(255, 200, 210, 255); // helles hellblau
+const Color wbButtonShadowGrey = Color.fromARGB(255, 150, 160, 200); // graublau
 const Color wbButtonBlue = Color.fromARGB(255, 32, 150, 243);
 const Color wbColorAppBarBlue =
     Color.fromARGB(255, 19, 83, 152); // dunkles blau
 const Color wbLogoBlue = Color.fromARGB(255, 4, 18, 114); // sehr dunkles blau
 
+// ----------------------------- Parbpalette grün ---
+const Color wbColorLightYellowGreen = Color.fromARGB(255, 250, 242, 242); // sehr helles Gelbgrün
+const Color wbColorButtonGreen = Color.fromARGB(255, 0, 130, 0); // sattes grün
+
+// ----------------------------- Parbpalette rot ---
+const Color wbColorButtonDarkRed = Color.fromRGBO(190, 13, 0, 1); // dunkles rot
+
+// ----------------------------- Parbpalette orange ---
 const Color wbDrawerLightOrange = Color.fromARGB(255, 255, 170, 0); // helles Orange
 const Color wbColorOrangeDarker = Color.fromARGB(255, 227, 139, 7); // dunkleres Orange
-
-const Color wbColorButtonGreen = Color.fromARGB(255, 0, 130, 0); // sattes grün
-const Color wbButtonShadowGrey = Color.fromARGB(255, 150, 160, 200); // graublau
-
-const Color wbColorButtonDarkRed = Color.fromRGBO(190, 13, 0, 1); // dunkles rot
